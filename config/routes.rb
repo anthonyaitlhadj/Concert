143a2eb5b24db2e0c01bc7c442637fd743f60b78
qkab78 <qkabasele@gmail.com>
2016-11-21 09:39:33 +0100

    message

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-<<<<<<< HEAD
-  root 'users#index'
+
+  root 'homes#index'
 
   get "signup" => "users#new", :as => "signup"
   get "logout" => "sessions#destroy", :as => "logout"
@@ -11,8 +11,5 @@ Rails.application.routes.draw do
   resources :sessions
   resources :comments
   resources :concerts
-=======
-  root 'homes#index'
   resources :homes
->>>>>>> 02a70c14ab17e38a8e28b610db60dce9770eebab
 end
