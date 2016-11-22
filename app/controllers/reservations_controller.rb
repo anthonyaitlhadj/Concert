14f62cc72aafb1493a3168e3994e548b997e6e48
anthonyaitlhadj <anthony.aitlhadj@ynov.com>
2016-11-22 11:45:18 +0100

    Reservation Ajout de la boucle pour les places dispo

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -14,9 +14,14 @@ class ReservationsController < ApplicationController
 
   def create
     @resa = Reservation.new(resa_params)
+    p "nb place dispo avant resa: #{@show.place}" 
     @resa.show = Show.find(params[:show_id])
+    @show.place -= @resa.nb_tickets
+
     if @resa.save #true ou false
+        @show.save
         redirect_to root_url
+        p "nb place dispo : #{@show.place}" 
     else
       render json: @resa.errors
     end
